0499d7027a9a722bbde87316d4c9154b7d3f3db0
Default User <default@email.com>
2023-09-30 05:49:16 +1000

    AUSTA-218: Added back in "Restart-Computer -Force" in post_install_setup.ps1.

--- files/postinstallsetuppowershellscript/contents/post_install_setup.ps1
+++ files/postinstallsetuppowershellscript/contents/post_install_setup.ps1
@@ -85,3 +85,8 @@ Set-Service WinRM -startuptype automatic
 
 # Update Hostname and Restart
 Rename-Computer -NewName '${newOsNode.hostname}' -Force -Restart
+
+# For some reason on Win10 BIOS ESXi kickstarts the restart from the above line
+# Doesn't restart. So we have another line below to restart the node
+# and start WinRM afterwards.
+Restart-Computer -Force
